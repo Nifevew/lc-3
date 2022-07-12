a7faa6e9a451d4fa77bf8d54b0b32469e2c173d6
Max <max.yablokov02@yandex.ru>
2022-07-12 21:33:22 +0300

    Добавлены объявления функций-команд. Реализованы некоторые команды.

--- lc-3/LC_3.cpp
+++ lc-3/LC_3.cpp
@@ -25,72 +25,72 @@ void LC_3::run()
 		{
 		case OPERATORS::ADD:
 		{
-
+			ADD(instruction);
 			break;
 		}
 		case OPERATORS::AND:
 		{
-
+			AND(instruction);
 			break;
 		}
 		case OPERATORS::NOT:
 		{
-
+			NOT(instruction);
 			break;
 		}
 		case OPERATORS::BR:
 		{
-
+			BR(instruction);
 			break;
 		}
 		case OPERATORS::JMP:
 		{
-
+			JMP(instruction);
 			break;
 		}
 		case OPERATORS::JSR:
 		{
-
+			JSR(instruction);
 			break;
 		}
 		case OPERATORS::LD:
 		{
-
+			LD(instruction);
 			break;
 		}
 		case OPERATORS::LDI:
 		{
-
+			LDI(instruction);
 			break;
 		}
 		case OPERATORS::LDR:
 		{
-
+			LDR(instruction);
 			break;
 		}
 		case OPERATORS::LEA:
 		{
-
+			LEA(instruction);
 			break;
 		}
 		case OPERATORS::ST:
 		{
-
+			ST(instruction);
 			break;
 		}
 		case OPERATORS::STI:
 		{
-
+			STI(instruction);
 			break;
 		}
 		case OPERATORS::STR:
 		{
-
+			STR(instruction);
 			break;
 		}
 		case OPERATORS::TRAP:
 		{
-
+			TRAP(instruction);
 			break;
 		}
 		case OPERATORS::RES:
@@ -107,7 +107,7 @@ void LC_3::run()
 }
 
 
-// TODO: возможно стоит сделать изменение значеиния через ссылку
+// TODO: возможно стоит сделать изменение значения через ссылку
 uint16_t LC_3::singExtend(uint16_t x, int bit_count) const 
 {
 	if (x >> (bit_count - 1) & 1)		//если первый бит 1 => число отрицательное, тогда число раширяется еденицами, а не нулями
@@ -135,7 +135,41 @@ void LC_3::updateFlags(uint16_t r)
 	}
 }
 
+
 void LC_3::ADD(uint16_t instruction)
+{
+	uint16_t r_0 = (instruction >> 9) & 0x7;						// адрес регистра результата
+	uint16_t r_1 = (instruction >> 6) & 0x7;						// адрес регистра первого слагаемого
+
+	uint16_t imm_flag = (instruction >> 5) & 0x1;
+
+	if (imm_flag == 1)
+	{
+		uint16_t imm5 = singExtend(instruction & 0x1F, 5); 
+		registers[r_0] = registers[r_1] + imm5;
+	}
+	else
+	{
+		uint16_t r_2 = instruction & 0x7;							// адрес регистра второго слагаемого
+		registers[r_0] = registers[r_1] + registers[r_2];
+	}
+
+	updateFlags(r_0);
+}
+
+
+void LC_3::LDI(uint16_t instruction)
+{
+	uint16_t r_0 = (instruction >> 9) & 0x7;
+	uint16_t pc_offset = singExtend(instruction & 0x1FFF, 9);
+
+	registers[r_0] = mem_read(registers[static_cast<int>(REGISTERS::PC)] + pc_offset);
+
+	updateFlags(r_0);
+}
+
+
+void LC_3::AND(uint16_t instruction)
 {
 	uint16_t r_0 = (instruction >> 9) & 0x7;
 	uint16_t r_1 = (instruction >> 6) & 0x7;
@@ -144,14 +178,19 @@ void LC_3::ADD(uint16_t instruction)
 
 	if (imm_flag == 1)
 	{
-		uint16_t imm5 = singExtend(instruction & 0x1F, 5);
-		registers[r_0] = registers[r_1] + imm5;
+		uint16_t imm5 = singExtend(instruction & 0x1F, 5); // TODO: what is 0x1F ???
 	}
 	else
 	{
 		uint16_t r_2 = instruction & 0x7;
-		registers[r_0] = registers[r_1] + registers[r_2];
+		registers[r_0] = registers[r_1] & registers[r_2];
 	}
 
 	updateFlags(r_0);
+}
+
+
+void LC_3::BR(uint16_t instruction)
+{
+
 }
--- lc-3/LC_3.h
+++ lc-3/LC_3.h
@@ -88,6 +88,19 @@ private:
 
 
 	void ADD(uint16_t instruction);
+	void LDI(uint16_t instruction);
+	void AND(uint16_t instruction);
+	void NOT(uint16_t instruction);
+	void BR(uint16_t instruction);
+	void JMP(uint16_t instruction);
+	void JSR(uint16_t instruction);
+	void LD(uint16_t instruction);
+	void LDR(uint16_t instruction);
+	void TRAP(uint16_t instruction);
+	void ST(uint16_t instruction);
+	void STI(uint16_t instruction);
+	void STR(uint16_t instruction);
+	void LEA(uint16_t instruction);
 };
 
 
