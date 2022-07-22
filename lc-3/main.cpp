557cc4fedea2a782ed6a26a0eef09071c2a10175
Max <max.yablokov02@yandex.ru>
2022-07-22 21:20:20 +0300

    Запуск или с помощью командной строки, либо через ввод в самой программе. Обработка исключений.

--- lc-3/main.cpp
+++ lc-3/main.cpp
@@ -1,7 +1,37 @@
-﻿
-int main()
+﻿#include "LC_3.h"
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
 {
+    std::string path_to_file;
 
+    if (argc == 2)
+    {
+        path_to_file = argv[1];
+    }
+    else
+    {
+        std::cout << "Enter a path to program file : ";
+        std::cin >> path_to_file;
+        system("cls");
+    }
 
+    try
+    {
+        LC_3 lc_3(path_to_file);
+
+        lc_3.run();
+    }
+    catch (std::runtime_error& re)
+    {
+        std::cerr << re.what() << std::endl;
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr << ex.what() << std::endl;
+    }
+    
     return 0;
 }
